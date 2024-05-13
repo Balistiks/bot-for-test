64ddfdb459dd7b108f75d437d89013271c09c2fd
balistik <balistiktw@gmail.com>
2024-05-13 18:59:02 +1000

    add back to docker

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'process';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return {
+          type: 'postgres',
+          host: 'db',
+          port: 5432,
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          entities: [Task],
+          synchronize: true,
+        };
+      },
+    }),
+    TasksModule,
+  ],
   controllers: [],
   providers: [],
 })
